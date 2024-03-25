ec8daa2e3f65912112e9035c982f87f78efab1c4
kota-aso <kotaaso1212@gmail.com>
2024-03-25 22:51:39 +0900

    Initial Commit

---
+++ MyPets/MyPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetsApp.swift
+//  MyPets
+//
+//  Created by Kota Aso on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
